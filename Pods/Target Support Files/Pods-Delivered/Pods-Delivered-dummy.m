94720b3f465262a7fce254fe6ea5a77728baea9d
anupam kumar <anupam.kumar@delhivery.com>
2016-01-06 19:04:35 +0530

    Setting Up Project Tree

---
+++ Pods/Target Support Files/Pods-Delivered/Pods-Delivered-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Delivered : NSObject
+@end
+@implementation PodsDummy_Pods_Delivered
+@end
---
+++ Pods/Target Support Files/Pods-DeliveredTests/Pods-DeliveredTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DeliveredTests : NSObject
+@end
+@implementation PodsDummy_Pods_DeliveredTests
+@end
